b734e028e2e60cfa1b0eb0dff290a803a9e54c63
ChrisLinOvO <qq2120621@gmail.com>
2020-06-21 20:37:12 +0800

    6/21-update

--- routes/article-routes.js
+++ routes/article-routes.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const {
   getArticleItems,
-  // getArticleCollection,
+ 
   getArticleItemById,
 } = require("../controllers/article-controllers");
 
 const router = express.Router();
 
 router.get("/", getArticleItems);
-// router.get("/:articles", getArticleCollection);
+
 router.get("/:articleId", getArticleItemById);
 
 module.exports = router;
