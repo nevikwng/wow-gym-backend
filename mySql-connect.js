2f422a4dd3839510adae29a491c3389569418758
Tim Chen <timchen@Tims-MacBook-Pro.local>
2020-06-21 15:03:40 +0800

    change DB config

--- mySql-connect.js
+++ mySql-connect.js
@@ -17,6 +17,7 @@ const mysql = require("mysql2");
 //   database: process.env.DB_TABLE,
 // });
 
+// production config
 const pool = mysql.createPool({
   host: "eu-cdbr-west-03.cleardb.net",
   user: "b7aafd89bf2851",
