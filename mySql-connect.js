c64cabbf257ff012d49dd3675e21f30b8bc72cf6
Tim Chen <timchen@Tims-MacBook-Pro.local>
2020-06-21 15:00:37 +0800

    change DB config

--- mySql-connect.js
+++ mySql-connect.js
@@ -10,14 +10,18 @@ const mysql = require("mysql2");
 //   queueLimit: 0,
 // });
 
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_TABLE,
+// });
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_TABLE,
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0,
+  host: "eu-cdbr-west-03.cleardb.net",
+  user: "b7aafd89bf2851",
+  password: "633fdffc",
+  database: "heroku_a97c1aaa6daa789",
 });
 
 module.exports = pool.promise();
