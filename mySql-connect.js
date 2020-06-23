9274a2b9f73c130d8a478b55f6e88a6dfa369fc7
Tim Chen <timchen@tims-mbp.iiiedu.local>
2020-06-23 17:45:26 +0800

    change DB config

--- mySql-connect.js
+++ mySql-connect.js
@@ -10,21 +10,19 @@ const mysql = require("mysql2");
 //   queueLimit: 0,
 // });
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_TABLE,
-// });
-
-// production config
 const pool = mysql.createPool({
-  host: "eu-cdbr-west-03.cleardb.net",
-  user: "b7aafd89bf2851",
-  password: "633fdffc",
-  database: "heroku_a97c1aaa6daa789",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_TABLE,
 });
 
-module.exports = pool.promise();
+// production config
+// const pool = mysql.createPool({
+//   host: "eu-cdbr-west-03.cleardb.net",
+//   user: "b7aafd89bf2851",
+//   password: "633fdffc",
+//   database: "heroku_a97c1aaa6daa789",
+// });
 
-//123
+module.exports = pool.promise();
