e73063d3ca48abaf16a0d0c3c3b5653f512a30b0
Tim Chen <timchen@tims-mbp.iiiedu.local>
2020-06-24 14:32:51 +0800

    finial version

--- mySql-connect.js
+++ mySql-connect.js
@@ -1,20 +1,18 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "wow-gym",
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0,
 
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "wow-gym",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_TABLE,
-
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_TABLE,
 });
 
 // production config
