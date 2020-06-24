abbf3ee7390696e6a80bad005a64876c525593f1
Tim Chen <timchen@tims-mbp.iiiedu.local>
2020-06-24 18:10:39 +0800

    fix member route bug

--- controllers/member-controllers.js
+++ controllers/member-controllers.js
@@ -4,47 +4,21 @@ const moment = require("moment-timezone");
 
 const getMember = async (req, res) => {
   const newRow = {};
-  const [rows] = await db.query(`SELECT * FROM member`);
-  if (rows) newRow.membersRow = rows;
-  // console.log(newRow.coursesRow)
-  for (i of rows) {
-    const fm = "ddd MM DD HH:mm";
-    i.courseTime = moment(i.courseTime).format(fm);
-    // console.log(i.courseTime)
+  try {
+    const [rows] = await db.query("SELECT * FROM user");
+    res.json(rows);
+  } catch (err) {
+    res.send(err);
   }
-  res.json(newRow);
 };
 
-// const getMemberID = async (req, res) => {
-//     try {
-//         const newRow = {}
-//         const memberId = req.params.memberId;
-//         console.log(memberID);
-//         const [row] = await db.query(`SELECT * FROM member WHERE memberId=${memberId}`);
-//         if (row) newRow.membersRow = row
-//         // console.log(newRow.coursesRow)
-//         for (i of row) {
-
-//             const fm = 'ddd MM DD HH:mm'
-//             i.courseTime = moment(i.courseTime).format(fm)
-//             // console.log(i.courseTime)
-//         }
-
-//         res.json(newRow);
-
-//     } catch (err) {
-//         return next(new HttpError("Can't find shop item", 404));
-//         console.log(err)
-//     }
-// };
-
-const getMemberId = async (req, res) => {
+const getMemberId = async (req, res, next) => {
   try {
     const newRow = {};
-    const memberId = req.params.memberId;
+    const userId = req.params.userId;
     //   console.log(memberId);
     const [rows] = await db.query(
-      `SELECT * FROM member WHERE memberId=${memberId}`
+      `SELECT * FROM user WHERE memberId=${userId}`
     );
     if (rows) newRow.membersRow = rows;
     // console.log(row)
@@ -59,34 +33,4 @@ const getMemberId = async (req, res) => {
   }
 };
 
-const getMemberLogin = async (req, res) => {
-  const newRow = {};
-  const [rows] = await db.query(
-    "SELECT `memberAccount`,`memberPwd`,`memberName`,`memberId`, `memberEmail`, `courseId`, `courseName`, `courseCategory`, `staffId`, `courseTime`, `courseHour` FROM `member`"
-  );
-  if (rows) newRow.memberRow = rows;
-  // console.log(newRow.coursesRow)
-  for (i of rows) {
-    const fm = "ddd MM DD HH:mm";
-    i.courseTime = moment(i.courseTime).format(fm);
-    // console.log(i.courseTime)
-  }
-  res.json(newRow);
-};
-
-const getMemberLoginId = async (req, res) => {
-  const newRow = {};
-  const [rows] = await db.query(
-    `SELECT * FROM member WHERE memberId=${memberId}`
-  );
-  if (rows) newRow.memberRow = rows;
-  // console.log(newRow.coursesRow)
-  for (i of rows) {
-    const fm = "ddd MM DD HH:mm";
-    i.courseTime = moment(i.courseTime).format(fm);
-    // console.log(i.courseTime)
-  }
-  res.json({ memberRow: newRow });
-};
-
-module.exports = { getMember, getMemberId, getMemberLogin, getMemberLoginId };
+module.exports = { getMember, getMemberId };
--- mySql-connect.js
+++ mySql-connect.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_TABLE,
+  host: "localhost",
+  user: "root",
+  password: "tim53685368",
+  database: "wow-gym",
 });
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_TABLE,
+// });
 
 // production config
 // const pool = mysql.createPool({
