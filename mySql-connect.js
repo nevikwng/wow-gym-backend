471f5fbd2c4ba8b11019c187108b3818e96fc512
Tim Chen <timchen@Tims-MacBook-Pro.local>
2020-06-26 19:42:21 +0800

    test

--- mySql-connect.js
+++ mySql-connect.js
@@ -7,19 +7,19 @@ const mysql = require("mysql2");
 //   database: "wow-gym",
 // });
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_TABLE,
-// });
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_TABLE,
+});
 
 // production config
-const pool = mysql.createPool({
-  host: "eu-cdbr-west-03.cleardb.net",
-  user: "b7aafd89bf2851",
-  password: "633fdffc",
-  database: "heroku_a97c1aaa6daa789",
-});
+// const pool = mysql.createPool({
+//   host: "eu-cdbr-west-03.cleardb.net",
+//   user: "b7aafd89bf2851",
+//   password: "633fdffc",
+//   database: "heroku_a97c1aaa6daa789",
+// });
 
 module.exports = pool.promise();
